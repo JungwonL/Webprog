99dbed0483d81fc97ea3b2950e5ff41f55d67f3d
DESKTOP-NS5GP3F\ITS_2 <leejw2247@naver.com>
2018-11-16 18:53:27 +0900

    init

--- Server/main.js
+++ Server/main.js
@@ -1,29 +1,44 @@
+/*init setting*/
+//module
 var express = require('express');
 var app = express();
-app.use(express.static('public')); //정적파일 서비스->수정후 서버 재실행 필요X
+var bodyParser = require('body-parser');
+//middleware
+app.use(express.static('../Client/public')); //정적파일 서비스->수정후 서버 재실행 필요X
+app.use(bodyParser.urlencoded({extended: false}));
+//pug
+app.locals.pretty = true;
+app.set('views', './views');
+app.set('view engine', 'pug'); //pug연결
+//root page
 app.get('/', function(req, res){
   res.send('<img src="/mainHeader.png">');
 });
+/**/
+
+//other page linkage
 app.get('/login', function(req, res){
   res.send('Login please');
 });
 app.get('/logout', function(req, res){
   res.send('Completed Logout');
 });
-app.get('/contect', function(req, res){
+app.get('/content', function(req, res){
   res.send('Write down your story');
+});
+app.get('/contents', function(req, res){
+  res.render('contents'); //form_receiver.pug -> contents.pug
+});
+app.get('/queryTest', function(req, res){ //query test
+  res.send(req.query.id+','+req.query.name);
 });
 app.listen(3000, function(){
   console.log('Connected 3000port!');
 });
 
-
-//(연결을 돕는)node.js -> fs, http, os
-//javascript통해 사용
-
-//(연결을 돕는)module (ex) express, underscore, jade
-//npm통해 module사용
-
-//get등 router통해 각 페이지와 연결
-
-//(연결을 돕는)controller
+//POST
+app.post('/contents_receiver', function(req, res){
+  var title = req.body.title;
+  var description = req.body.description;
+  res.send('title is '+title+'</p>discription is '+description);
+});
